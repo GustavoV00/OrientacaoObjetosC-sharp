b1ab958cedbdd0b32ed798ddfc6d4227a1e37d2a
Gustavo Valente <gustavo.pw00@gmail.com>
2023-09-26 15:05:10 -0300

    completo

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CelularPOO.Models;
+
+Iphone iphone = new Iphone("numero", "iphone", "imei", "memoria");
+Nokia nokia = new Nokia("numero", "nokia", "imei", "memoria");
+
+iphone.InstalarAplicativo("facebook");
+nokia.InstalarAplicativo("instagram");
+
+iphone.Ligar();
+nokia.Ligar();
+
+iphone.ReceberLigacao();
+nokia.ReceberLigacao();
